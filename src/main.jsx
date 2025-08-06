9f6a5f49f375825674d76e4ffef6f53ba3e61475
Sasha Shevchenko <acidi@icloud.com>
2025-08-06 13:38:14 -0700

    inits mock server

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes.jsx";
+import { makeServer } from "./services/mockServer.js";
+
+// Start the mock server
+makeServer();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
